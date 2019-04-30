0e63d7007a9020216f31850b03f214bc07d0c470
Kwinnieprince <kwinten@delrue.com>
2019-04-30 20:21:49 +0200

    updates

--- src/controller/GetUsersAngular.java
+++ src/controller/GetUsersAngular.java
@@ -1,12 +1,35 @@
 package controller;
 
+import domain.Person;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 public class GetUsersAngular extends RequestHandler {
     @Override
     public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
         Controller.setSendJson();
         return toJSON(getPersonService().getPersons());
+    }
+
+    public String toJSON(List<Person> persons){
+        String json = "[";
+        int i = 0;
+        for (Person person : persons){
+            json += "{\"userId\":\"" + person.getUserId() + "\"," +
+                    "\"password\":\"" + person.getPassword() + "\"," +
+                    "\"salt\":\"" + person.getSalt() + "\"," +
+                    "\"firstName\":\"" + person.getFirstName() + "\"," +
+                    "\"lastName\":\"" + person.getLastName() + "\"," +
+                    "\"role\":\"" + person.getRole() + "\"," +
+                    "\"status\":\"" + person.getStatus() + "\"}";
+            i++;
+            if (persons.size() != i){
+                json += ",";
+            }
+        }
+        json += "]";
+        return json;
     }
 }
